cfc5368247d383a9dc1a21f4766f32c2deb4d17c
youssief101 <youssiefmok@gmail.com>
2026-07-30 18:59:07 +0300

    Testing all modules, controllers, and routes of ali's

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
-const connectDB = async()=> {
+
+const connectDB = async () => {
+
+    console.log(process.env.MONGODB_URI);
+
     try {
+
         await mongoose.connect(process.env.MONGODB_URI);
+
         console.log("MongoDB connected successfully");
-    } catch(error) {
-        console.error("MongoDB connection failed");
-        console.error(error.message);
+
+    } catch (error) {
+
+        console.error(error);
+
         process.exit(1);
+
     }
-}
+
+};
 
 module.exports = connectDB;
-
-// Advantages of isolating database.js:
-
-// Single responsibility
-// Easier maintenance
-// Cleaner startup logic
-// Easy to reuse in testing
