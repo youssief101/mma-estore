6d08b65aea686ae677274940700ee7dc0c099e0c
youssief101 <youssiefmok@gmail.com>
2026-08-02 06:47:12 +0300

    chore(frontend): scaffold Angular application architecture

---
+++ mma-estore-frontend/src/app/features/wishlist/pages/wishlist/wishlist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist',
+  imports: [],
+  templateUrl: './wishlist.html',
+  styleUrl: './wishlist.css',
+})
+export class Wishlist {
+
+}
