a4ac5bbd7f42d9b5a7684d753ed8878cefe8bfce
youssief101 <youssiefmok@gmail.com>
2026-08-02 20:56:20 +0300

    feat(auth): add authentication login page

--- mma-estore-frontend/src/app/app.routes.ts
+++ mma-estore-frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './features/auth/pages/login/login';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    component: Login
+  }
+
+];
--- mma-estore-frontend/src/app/features/auth/pages/login/login.ts
+++ mma-estore-frontend/src/app/features/auth/pages/login/login.ts
@@ -1,11 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
+
+  private fb = inject(FormBuilder);
+
+  private authService = inject(AuthService);
+
+  private router = inject(Router);
+
+  loading = false;
+
+  errorMessage = '';
+
+  loginForm = this.fb.group({
+
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+
+    password: [
+      '',
+      [
+        Validators.required
+      ]
+    ]
+
+  });
+
+  onSubmit(): void {
+
+    if (this.loginForm.invalid) {
+
+      this.loginForm.markAllAsTouched();
+
+      return;
+
+    }
+
+    this.loading = true;
+
+    this.errorMessage = '';
+
+    this.authService.login({
+
+      email: this.loginForm.value.email!,
+
+      password: this.loginForm.value.password!
+
+    }).subscribe({
+
+      next: () => {
+
+        this.loading = false;
+
+        this.router.navigate(['/']);
+
+      },
+
+      error: (err) => {
+
+        this.loading = false;
+
+        this.errorMessage =
+          err.error?.message ??
+          'Login failed.';
+
+      }
+
+    });
+
+  }
 
 }
