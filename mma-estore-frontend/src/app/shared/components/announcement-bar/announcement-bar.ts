5b5c98891a95c7ae6932dfd9000064c12f1cd535
youssief101 <youssiefmok@gmail.com>
2026-08-03 14:10:10 +0300

    implementing phase 7.4.11 — Announcement Bar

--- mma-estore-frontend/src/app/shared/components/announcement-bar/announcement-bar.ts
+++ mma-estore-frontend/src/app/shared/components/announcement-bar/announcement-bar.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-announcement-bar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './announcement-bar.html',
-  styleUrl: './announcement-bar.css',
+  styleUrl: './announcement-bar.css'
 })
 export class AnnouncementBar {
+
+  @Input()
+  message =
+    'FREE SHIPPING ON ORDERS OVER $99';
+
+  @Input()
+  background =
+    'var(--color-primary)';
+
+  @Input()
+  color =
+    '#ffffff';
 
 }
--- mma-estore-frontend/src/app/shared/components/navbar/navbar.ts
+++ mma-estore-frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,77 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
 export class Navbar {
+
+  categories = [
+
+    {
+      title: 'UFC Venum',
+      link: '/venum'
+    },
+
+    {
+      title: 'Fighters',
+      link: '/fighters'
+    },
+
+    {
+      title: 'Events',
+      link: '/events'
+    },
+
+    {
+      title: 'Apparel',
+      link: '/products/apparel'
+    },
+
+    {
+      title: 'T-Shirts',
+      link: '/products/t-shirts'
+    },
+
+    {
+      title: 'Hats',
+      link: '/products/hats'
+    },
+
+    {
+      title: 'Equipment',
+      link: '/products/equipment'
+    },
+
+    {
+      title: 'Replica Belts',
+      link: '/products/replica-belts'
+    },
+
+    {
+      title: 'Collectibles',
+      link: '/products/collectibles'
+    },
+
+    {
+      title: 'Home & Office',
+      link: '/products/home-office'
+    },
+
+    {
+      title: 'Sale',
+      link: '/sale'
+    }
+
+  ];
 
 }
