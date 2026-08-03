24e60bc2fe78ba8da259aed52c1cdee8a8e47558
youssief101 <youssiefmok@gmail.com>
2026-08-03 12:55:30 +0300

    implement the Card Foundation

--- mma-estore-frontend/src/app/shared/components/card/card.ts
+++ mma-estore-frontend/src/app/shared/components/card/card.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  input
+} from '@angular/core';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
   templateUrl: './card.html',
-  styleUrl: './card.css',
+  styleUrl: './card.css'
 })
 export class Card {
+
+  readonly hover =
+    input(true);
+
+  readonly bordered =
+    input(false);
+
+  readonly clickable =
+    input(false);
+
+  readonly shadow =
+    input<'none' | 'sm' | 'md' | 'lg'>('md');
+
+  readonly padding =
+    input<'none' | 'sm' | 'md' | 'lg'>('md');
 
 }
