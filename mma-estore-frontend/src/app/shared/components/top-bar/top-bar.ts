369249cbd2c17e376255f8029843bb4e17cc5db5
youssief101 <youssiefmok@gmail.com>
2026-08-03 13:14:08 +0300

    implementing phase 7.4.7 Top Bar, adding global css tokens

--- mma-estore-frontend/src/app/shared/components/top-bar/top-bar.ts
+++ mma-estore-frontend/src/app/shared/components/top-bar/top-bar.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+
+import { RouterLink } from '@angular/router';
+
 @Component({
+
   selector: 'app-top-bar',
-  imports: [],
+
+  standalone: true,
+
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
+
   templateUrl: './top-bar.html',
-  styleUrl: './top-bar.css',
+
+  styleUrl: './top-bar.css'
+
 })
+
 export class TopBar {
 
 }
