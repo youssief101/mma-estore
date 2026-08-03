1eed1ed5283d87d8880423b4f7829d2e5c06947e
youssief101 <youssiefmok@gmail.com>
2026-08-03 14:19:21 +0300

    implementing phase 7.4.14 — Auth Layout

--- mma-estore-frontend/src/app/app.routes.ts
+++ mma-estore-frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { Login } from './features/auth/pages/login/login';
 import { Register } from './features/auth/pages/register/register';
+import { AuthLayout } from './layouts/auth-layout/auth-layout';
 
 export const routes: Routes = [
 
@@ -12,13 +13,18 @@ export const routes: Routes = [
   },
 
   {
-    path: 'login',
-    component: Login
-  },
-
-  {
-    path: 'register',
-    component: Register
+      path:'',
+      component: AuthLayout,
+      children:[
+          {
+              path:'login',
+              component:Login
+          },
+          {
+              path:'register',
+              component:Register
+          }
+      ]
   }
 
 ];
--- mma-estore-frontend/src/app/layouts/auth-layout/auth-layout.ts
+++ mma-estore-frontend/src/app/layouts/auth-layout/auth-layout.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink
+  ],
   templateUrl: './auth-layout.html',
-  styleUrl: './auth-layout.css',
+  styleUrl: './auth-layout.css'
 })
-export class AuthLayout {
-
-}
+export class AuthLayout {}
