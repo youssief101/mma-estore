afe2c6cdc9c3969179ca8fdf029b714dfdff66d3
youssief101 <youssiefmok@gmail.com>
2026-08-03 14:13:29 +0300

    implementing phase 7.4.13 - Store Layout

--- mma-estore-frontend/src/app/layouts/store-layout/store-layout.ts
+++ mma-estore-frontend/src/app/layouts/store-layout/store-layout.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { TopBar } from '../../shared/components/top-bar/top-bar';
+import { AnnouncementBar } from '../../shared/components/announcement-bar/announcement-bar';
+import { Header } from '../../shared/components/header/header';
+import { Navbar } from '../../shared/components/navbar/navbar';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-store-layout',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    TopBar,
+    AnnouncementBar,
+    Header,
+    Navbar,
+    Footer
+  ],
   templateUrl: './store-layout.html',
-  styleUrl: './store-layout.css',
+  styleUrl: './store-layout.css'
 })
-export class StoreLayout {
-
-}
+export class StoreLayout {}
