9b4ba2621c68548662611ec1630207d2e07fa17e
mohmednassar <mohmednassar401@gmail.com>
2026-08-06 11:13:44 +0300

    Add shipping page

---
+++ mma-estore-frontend/src/app/pages/shipping/shipping.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-shipping',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shipping.html',
+  styleUrl: './shipping.css',
+})
+export class Shipping {}
