58db4e7c55ba362764507c0dcfba00d0da3c9a69
mohmednassar <mohmednassar401@gmail.com>
2026-08-06 11:13:44 +0300

    Add FAQ page

---
+++ mma-estore-frontend/src/app/pages/faq/faq.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-faq',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './faq.html',
+  styleUrl: './faq.css',
+})
+export class Faq {}
