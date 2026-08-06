0ba0a60fcfe69d3c0b4e0014ebe047497bd70ba6
mohmednassar <mohmednassar401@gmail.com>
2026-08-06 11:13:44 +0300

    Add returns page

---
+++ mma-estore-frontend/src/app/pages/returns/returns.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-returns',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './returns.html',
+  styleUrl: './returns.css',
+})
+export class Returns {}
