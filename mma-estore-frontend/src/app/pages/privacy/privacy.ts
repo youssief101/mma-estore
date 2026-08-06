cbb3a0a54e347a36e6776b9b8ee002c16b413a9d
mohmednassar <mohmednassar401@gmail.com>
2026-08-06 11:13:44 +0300

    Add privacy policy page

---
+++ mma-estore-frontend/src/app/pages/privacy/privacy.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-privacy',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './privacy.html',
+  styleUrl: './privacy.css',
+})
+export class Privacy {}
