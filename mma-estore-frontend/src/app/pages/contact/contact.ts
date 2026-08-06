a050b88602ce346fc8427584c59cbb5e0dad4cd6
mohmednassar <mohmednassar401@gmail.com>
2026-08-06 11:13:43 +0300

    Add contact page

---
+++ mma-estore-frontend/src/app/pages/contact/contact.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './contact.html',
+  styleUrl: './contact.css',
+})
+export class Contact {}
