68f4ae39e386a4df74fea3947c5e23d04f5fd02f
mohmednassar <mohmednassar401@gmail.com>
2026-08-06 10:48:27 +0300

    Add gift cards placeholder page

--- mma-estore-frontend/src/app/features/gift-cards/gift-cards.ts
+++ mma-estore-frontend/src/app/features/gift-cards/gift-cards.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-gift-cards',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './gift-cards.html',
   styleUrl: './gift-cards.css',
 })
-export class GiftCards {
-
-}
+export class GiftCards {}
