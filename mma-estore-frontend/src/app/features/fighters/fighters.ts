aff80c8d87837a3444e761b374fe88c66ee62019
mohmednassar <mohmednassar401@gmail.com>
2026-08-06 10:48:13 +0300

    Add fighters placeholder page

--- mma-estore-frontend/src/app/features/fighters/fighters.ts
+++ mma-estore-frontend/src/app/features/fighters/fighters.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-fighters',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './fighters.html',
   styleUrl: './fighters.css',
 })
-export class Fighters {
-
-}
+export class Fighters {}
