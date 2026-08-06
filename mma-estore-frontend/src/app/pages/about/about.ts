b86f956b9c310cddb231f3ff2d44e8a369932f2d
mohmednassar <mohmednassar401@gmail.com>
2026-08-06 10:46:57 +0300

    Add about page

---
+++ mma-estore-frontend/src/app/pages/about/about.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About {}
